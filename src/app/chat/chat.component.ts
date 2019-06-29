2f6f57c8db6a12199ffd63526681fa8c435eb334
arsley <blue20will@gmail.com>
2019-06-29 11:10:17 +0900

    :pencil: Create base template with mock data

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { chats }  from './chat_mock';
+
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.styl']
 })
 export class ChatComponent implements OnInit {
+  chats = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.fetchChat();
+  }
+
+  // rewrite REAL fetching
+  private fetchChat() {
+    this.chats = chats;
   }
 
 }
---
+++ src/app/chat/chat_mock.ts
@@ -0,0 +1,14 @@
+export const chats = [
+  {
+    name: 'user0',
+    body: 'hello 0-world',
+  },
+  {
+    name: 'user1',
+    body: 'hello 1-world',
+  },
+  {
+    name: 'user2',
+    body: 'hello 2-world',
+  },
+];
