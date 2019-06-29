65c870222304103ecb168aeda022bcab0ea3eee3
arsley <blue20will@gmail.com>
2019-06-29 10:55:17 +0900

    :zap: Create Chat component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ChatComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ChatComponent]
 })
 export class AppModule { }
